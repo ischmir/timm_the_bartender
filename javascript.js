2a84903389fa90912fb68e221373971b90a4a164
ischmir <timmhinsch@gmail.com>
2022-02-08 08:17:19 +0100

    update

---
+++ javascript.js
@@ -0,0 +1,8 @@
+console.log ("javascript running");
+//document.body.innerHTML ="<h1>ny tekst<h1>";
+    var hest = document.getElementById("1hest").innerHTML;
+    var hengst = document.getElementById("2hengst").innerHTML;
+    var farve = "pink";
+    var pony = " " + hest + " " + hengst;
+    pony.innerHTML = ("3resultat");
+    pony.style.color = "pink";
